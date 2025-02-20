5de19211cc776ae3304ef96b05319fee353f49fd
Dimitrios Psychogyios <dimitris@panda-surgical.com>
2025-02-20 22:26:29 +0000

    add fps lock

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame
 from constants import *
 
+FPS=60
+
 def main():
     print('Starting asteroids!')
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
     pygame.init()
+    pgc = pygame.time.Clock()
+    dt_s = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     while(True):
@@ -18,6 +22,9 @@ def main():
         
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        
+        # wait for 1 frame and compute delta time
+        dt_s = pgc.tick(FPS)/1000
 
 
 if __name__ == '__main__':
