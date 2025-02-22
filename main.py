300982da050695750d1199a880f3abea38f24327
Dimitrios Psychogyios <dimitris@panda-surgical.com>
2025-02-22 09:55:43 +0000

    implement asteroid fields

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 from constants import *
 import player as plyr
-
+import asteroid as astr
+from asteroidfield import AsteroidField
 
 def main():
     print('Starting asteroids!')
@@ -14,9 +15,14 @@ def main():
     
     updateables_g = pygame.sprite.Group()
     drawables_g = pygame.sprite.Group()
+    asteroirds_g = pygame.sprite.Group()
+    
+    astr.Asteroid.containers = updateables_g, drawables_g, asteroirds_g
+    AsteroidField.containers = updateables_g
     plyr.Player.containers = updateables_g, drawables_g
     
     player = plyr.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
     
     while(True):
         
